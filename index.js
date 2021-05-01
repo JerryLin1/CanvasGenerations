9522f2c59fc3d755acb46a34c4594eb0b25d09b3
JerryLin1 <jerreelynn@gmail.com>
2021-05-01 12:00:32 -0400

    color now on separate canvas

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var cCtx;
 
 var col;
 var row;
+var afps = 60;
 var fps = 1;
 var randDens = 0.50;
 var useColor = true;
@@ -35,7 +36,7 @@ const smiley = [
     [0, 0, 0, 0, 0, 0]
 ];
 var arr = smiley;
-var arrC;
+var clrs = [];
 
 const colorBlank = "rgba(0, 0, 0, 0)"
 const colorHistory = "rgba(2, 64, 150, 1)"
@@ -48,7 +49,7 @@ window.onload = function Init() {
     cW = document.getElementById("canWrapper");
 
     // Runs at 60fps (cannot be edited), but actual iterations per second can be adjusted (i still call fps)
-    setTimeout(Update, 1000 / 60);
+    setTimeout(Update, 1000 / afps);
     setTimeout(IterateMethod, 1000 / fps);
     inpCol = document.getElementById("col");
     inpRow = document.getElementById("row")
@@ -69,6 +70,7 @@ function Generate() {
     RedrawAll();
 }
 function RedrawAll() {
+    mCtx.clearRect(0, 0, mCan.width, mCan.height);
     for (let r = 0; r < row; r++) {
         for (let c = 0; c < col; c++) {
             let t = arr[r][c];
@@ -83,26 +85,38 @@ function RedrawAll() {
                     mCtx.fillRect(c * tileSize, r * tileSize, tileSize, tileSize);
                     break;
             }
-            if (arrC != null && useColor === true) {
-                mCtx.fillStyle = arrC[r][c];
-                mCtx.fillRect(c * tileSize, r * tileSize, tileSize, tileSize);
-                // if history blue color fade it away
-                if (ChangeTransparency(arrC[r][c], 1) === colorHistory) {
-                    arrC[r][c] = DecreaseTransparency(arrC[r][c]);
-                }
+        }
+    }
+}
+function DrawColors() {
+    cCtx.clearRect(0, 0, cCan.width, cCan.height);
+    for (let i = clrs.length - 1; i >= 0; i--) {
+        let r = clrs[i].key[0];
+        let c = clrs[i].key[1];
+        let clr = clrs[i].value;
+        if (r == 1 && c == 2) console.log(clr);
+
+        cCtx.fillStyle = clr;
+        cCtx.fillRect(c * tileSize, r * tileSize, tileSize, tileSize);
+
+        // if history blue color fade it away
+        if (ChangeTransparency(clr, 1) === colorHistory) {
+            clr = DecreaseTransparency(clr)
+            SetColor(r, c, clr);
+            clrs.splice(i, 1)
+            if (GetTransparency(clr) <= 0) {
+                clrs.splice(i, 1);
             }
         }
     }
 }
-function DrawColours() {
-
-}
 
 function Update() {
-    mCtx.clearRect(0, 0, mCan.width, mCan.height);
+    //move draw behaviour to iterate
     RedrawAll();
+    if (useColor) DrawColors();
 
-    setTimeout(Update, 1000 / 60);
+    setTimeout(Update, 1000 / afps);
 }
 function StartMethod() {
     switch (cgen) {
@@ -169,7 +183,7 @@ function StartBinaryTree() {
     arr[1][1] = 0;
     bar = 1;
     bac = 1;
-    arrC[bar][bac] = colorStart;
+    SetColor(bar, bac, colorStart);
 }
 function IterateBinaryTree() {
     let n = new Array();
@@ -183,16 +197,16 @@ function IterateBinaryTree() {
         arr[n[0][0]][n[0][1]] = 0;
         arr[bar][bac] = 0;
 
-        arrC[n[0][0]][n[0][1]] = colorHistory
-        arrC[bar][bac] = colorHistory;
+        SetColor(n[0][0], n[0][1], colorHistory);
+        SetColor(bar, bac, colorHistory);
     }
     else if (n.length === 2) {
         let rand = GetRndInt(0, 2);
         arr[n[rand][0]][n[rand][1]] = 0;
         arr[bar][bac] = 0;
 
-        arrC[n[rand][0]][n[rand][1]] = colorHistory
-        arrC[bar][bac] = colorHistory;
+        SetColor(n[rand][0], n[rand][1], colorHistory);
+        SetColor(bar, bac, colorHistory);
     }
     else {
         // nothing
@@ -201,7 +215,7 @@ function IterateBinaryTree() {
     if (bac > col - 1) {
         bar += 2;
         if (bar > row - 1) {
-            arrC[bar - 2][bac - 2] = colorStart;
+            SetColor(bar - 2, bac - 2, colorStart);
             cgen = gen.none;
         }
         bac = 1;
@@ -252,7 +266,7 @@ function UpdateOptions() {
     mCan.width = tcols;
     mCan.height = trows;
     cCan.width = tcols;
-    mCan.height = trows;
+    cCan.height = trows;
 
     fps = parseInt(inpFps.value);
     useColor = inpColour.checked;
@@ -281,10 +295,16 @@ function GetGenType() {
 }
 function NewArr() {
     arr = Array.from(Array(row), _ => Array(col).fill(0));
-    arrC = Array.from(Array(row), _ => Array(col).fill(colorBlank));
+    ResetArrC();
 }
 function ResetArrC() {
-    arrC = Array.from(Array(row), _ => Array(col).fill(colorBlank));
+    clrs = [];
+}
+function SetColor(r, c, color) {
+    clrs.push({
+        key: [r, c],
+        value: color
+    });
 }
 function GetRndInt(min, max) { return Math.floor(Math.random() * (max - min)) + min; }
 function DecreaseTransparency(rgba) {
